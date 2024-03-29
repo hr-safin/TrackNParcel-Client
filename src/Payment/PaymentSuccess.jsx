de82ced20846e23e7644a27e8bec872562db2565
safinhr <hrsafin2434@gmail.com>
2024-03-30 01:05:59 +0600

    confetti responsive issue solved

--- src/Payment/PaymentSuccess.jsx
+++ src/Payment/PaymentSuccess.jsx
@@ -1,17 +1,20 @@
 import { useWindowSize } from "@react-hook/window-size";
 import React from "react";
-
 import Confetti from "react-confetti";
+
 const PaymentSuccess = () => {
-    const { width, height } = useWindowSize()
-  return (
-    <div>
-        <div className=" flex text-center w-full text-3xl md:text-4xl lg:text-5xl justify-center items-center h-[100vh]">
-            Payment Successful
+    const { width, height } = useWindowSize();
+    
+    
+
+    return (
+        <div>
+            <div className="flex text-center w-full text-3xl md:text-4xl lg:text-5xl justify-center items-center h-[100vh]">
+                Payment Successful
+            </div>
+             <Confetti className=" h-screen  w-full" width={width} height={height}/>
         </div>
-      <Confetti width={width} height={height} />
-    </div>
-  );
+    );
 };
 
 export default PaymentSuccess;
